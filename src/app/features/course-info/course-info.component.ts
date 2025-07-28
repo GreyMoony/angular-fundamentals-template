95cf41908c591a42e002e268acdb57bfd7da14b8
Сергій <sergvoy1996@gmail.com>
2025-07-28 15:54:34 +0300

    task?

--- src/app/features/course-info/course-info.component.ts
+++ src/app/features/course-info/course-info.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+export interface Course {
+  id: string;
+  title: string;
+  description: string;
+  creationDate: Date;
+  duration: number;
+  authors: string[];
+}
 @Component({
   selector: 'app-course-info',
   templateUrl: './course-info.component.html',
   styleUrls: ['./course-info.component.scss']
 })
 export class CourseInfoComponent {
-  // Use the names for the input `course`.
+  @Input() course!: Course;
 }
---
+++ src/app/features/courses/courses-list/courses-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+export interface Course {
+  id: string;
+  title: string;
+  description: string;
+  creationDate: Date;
+  duration: number;
+  authors: string[];
+}
+
+@Component({
+  selector: 'app-courses-list',
+  templateUrl: './courses-list.component.html',
+  styleUrls: ['./courses-list.component.css']
+})
+export class CoursesListComponent {
+  @Input() courses: Course[] = [];
+  @Input() editable: boolean = false;
+
+  @Output() showCourse = new EventEmitter<string>();
+  @Output() editCourse = new EventEmitter<string>();
+  @Output() deleteCourse = new EventEmitter<string>();
+
+  onShow(id: string): void {
+    this.showCourse.emit(id);
+  }
+
+  onEdit(id: string): void {
+    this.editCourse.emit(id);
+  }
+
+  onDelete(id: string): void {
+    this.deleteCourse.emit(id);
+  } 
+}
