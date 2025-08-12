a3989d532dab6d15b9d8ecceb5f1915e3f04334d
Сергій <sergvoy1996@gmail.com>
2025-08-12 19:10:00 +0300

    fix...

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,1 +1,12 @@
 import 'jest-preset-angular/setup-jest';
+import { TestBed } from '@angular/core/testing';
+import { WINDOW } from '@app/auth/services/window.token';
+
+// Provide the WINDOW token globally for all tests
+beforeEach(() => {
+  TestBed.configureTestingModule({
+    providers: [
+      { provide: WINDOW, useValue: window }
+    ]
+  });
+});
