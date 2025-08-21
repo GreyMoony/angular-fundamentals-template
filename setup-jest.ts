792ff8f8643cfdb6da923e877640bf18015bf28b
Сергій <sergvoy1996@gmail.com>
2025-08-21 17:03:30 +0300

    fix nullInjectorError for tests

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,12 +1,24 @@
 import 'jest-preset-angular/setup-jest';
 import { TestBed } from '@angular/core/testing';
 import { WINDOW } from '@app/auth/services/window.token';
+import { provideMockStore } from '@ngrx/store/testing';
 
 // Provide the WINDOW token globally for all tests
 beforeEach(() => {
   TestBed.configureTestingModule({
     providers: [
-      { provide: WINDOW, useValue: window }
+      { provide: WINDOW, useValue: window },
+      provideMockStore({
+        initialState: {
+          courses: {
+            allCourses: [],
+            isAllCoursesLoading: false,
+            isSingleCourseLoading: false,
+            isSearchState: false,
+            errorMessage: null,
+          },
+        },
+      }),
     ]
   });
 });
